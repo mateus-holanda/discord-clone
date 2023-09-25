1d6c72280f145fdf10d02c097c44f212c544cbfc
Mateus Holanda <mateus23ita@gmail.com>
2023-09-24 23:46:16 -0300

    Create navigation sidebar with available servers

---
+++ components/navigation/NavigationItem.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import Image from "next/image";
+import { useParams, useRouter } from "next/navigation";
+
+import { ActionTooltip } from "@/components/ActionTooltip";
+import { cn } from "@/lib/utils";
+
+interface NavigationItemProps {
+  id: string;
+  name: string;
+  imageUrl: string;
+}
+
+export function NavigationItem({ id, name, imageUrl }: NavigationItemProps) {
+  const params = useParams();
+  const router = useRouter();
+
+  return (
+    <ActionTooltip
+      side="right"
+      align="center"
+      label={name}
+    >
+      <button
+        className="group relative flex items-center"
+        onClick={() => router.push(`/servers/${id}`)}
+      >
+        <div
+          className={cn(
+            "absolute left-0 bg-primary rounded-r-full transition-all w-[4px]",
+            params?.serverId !== id && "group-hover:h-[20px]",
+            params?.serverId === id ? "h-[36px]" : "h-[8px]",
+          )}
+        />
+
+        <div
+          className={cn(
+            "relative group flex mx-3 h-[48px] w-[48px] rounded-[24px] group-hover:rounded-[16px] transition-all overflow-hidden",
+            params?.serverId === id && "bg-primary/10 text-primary rounded-[16px]",
+          )}
+        >
+          <Image src={imageUrl} alt="Channel" fill />
+        </div>
+      </button>
+    </ActionTooltip>
+  )
+}
